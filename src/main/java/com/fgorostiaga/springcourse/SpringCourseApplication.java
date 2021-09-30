c5a813d693d853668094f2f1d395bec4ba7efe50
Felipe Gorostiaga <fgorostiaga@itba.edu.ar>
2021-09-30 13:15:19 -0300

    Added swagger documentation

--- src/main/java/com/fgorostiaga/springcourse/SpringCourseApplication.java
+++ src/main/java/com/fgorostiaga/springcourse/SpringCourseApplication.java
@@ -3,10 +3,16 @@ package com.fgorostiaga.springcourse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @SpringBootApplication
@@ -21,6 +27,14 @@ public class SpringCourseApplication {
 		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
+	}
+
+	@Primary
+	@Bean
+	public LinkDiscoverers discoverers() {
+		List<LinkDiscoverer> plugins = new ArrayList<>();
+		plugins.add(new CollectionJsonLinkDiscoverer());
+		return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
 	}
 
 }
